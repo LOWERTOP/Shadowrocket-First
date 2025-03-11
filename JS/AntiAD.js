6bb26e18cb0eada69c854cf89230a529ea1202b8
LOWERTOP <onone.so@gmail.com>
2025-03-11 19:38:47 +0800

    Update AntiAD.js

--- JS/AntiAD.js
+++ JS/AntiAD.js
@@ -9,19 +9,19 @@ const body = $response.body;
 let modifiedBody = body;
 
 // 删除 .mobadsq 广告
-modifiedBody = modifiedBody.replace(/<div[^>]*\bmobadsq\b[^>]*>[.\s\S]*?<\/div>/gi, '');
+modifiedBody = modifiedBody.replace(/<div[^>]*\bmobadsq\b[^>]*>[\s\S]*?<\/div>/gi, '');
 
 // 删除 .div_sub_adhost 广告
-modifiedBody = modifiedBody.replace(/<div[^>]*\bdiv_sub_adhost\b[^>]*>[.\s\S]*?<\/div>/gi, '');
+modifiedBody = modifiedBody.replace(/<div[^>]*\bdiv_sub_adhost\b[^>]*>[\s\S]*?<\/div>/gi, '');
 
 // 删除 .div_adhost 广告
-modifiedBody = modifiedBody.replace(/<div[^>]*\bdiv_adhost\b[^>]*>[.\s\S]*?<\/div>/gi, '');
+modifiedBody = modifiedBody.replace(/<div[^>]*\bdiv_adhost\b[^>]*>[\s\S]*?<\/div>/gi, '');
 
 // 删除 .div_close_ads 广告
-modifiedBody = modifiedBody.replace(/<div[^>]*\bdiv_close_ads\b[^>]*>[.\s\S]*?<\/div>/gi, '');
+modifiedBody = modifiedBody.replace(/<div[^>]*\bdiv_close_ads\b[^>]*>[\s\S]*?<\/div>/gi, '');
 
 // 删除 .l-box 广告
-modifiedBody = modifiedBody.replace(/<div[^>]*\bl-box\b[^>]*>[.\s\S]*?<\/div>/gi, '');
+modifiedBody = modifiedBody.replace(/<div[^>]*\bl-box\b[^>]*>[\s\S]*?<\/div>/gi, '');
 
 // 返回修改后的 HTML
 $done({ body: modifiedBody });
