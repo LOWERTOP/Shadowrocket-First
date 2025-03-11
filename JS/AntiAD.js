a16d9925191f70cc1e7673469183be46b5290754
LOWERTOP <onone.so@gmail.com>
2025-03-11 19:11:37 +0800

    Update AntiAD.js

--- JS/AntiAD.js
+++ JS/AntiAD.js
@@ -1,6 +1,6 @@
 /**
  * Shadowrocket 移除 twmanga 相关网站的广告元素
- * 目标：删除 class="mobadsq", "div_sub_adhost", "div_adhost", "div_close_ads" 的广告
+ * 目标：删除 class="mobadsq", "div_sub_adhost", "div_adhost", "div_close_ads", "l-box" 的广告
  * 适用于 twmanga 的所有域名
  */
 
@@ -8,17 +8,14 @@
 const body = $response.body;
 let modifiedBody = body;
 
-// 删除 .mobadsq 广告
-modifiedBody = modifiedBody.replace(/<div[^>]*class=["']?mobadsq["']?[^>]*>.*?<\/div>/g, '');
+// 需要删除的广告类名列表
+const adClasses = ["mobadsq", "div_sub_adhost", "div_adhost", "div_close_ads", "l-box"];
 
-// 删除 .div_sub_adhost 广告
-modifiedBody = modifiedBody.replace(/<div[^>]*class=["']?div_sub_adhost["']?[^>]*>.*?<\/div>/g, '');
-
-// 删除 .div_adhost 广告
-modifiedBody = modifiedBody.replace(/<div[^>]*class=["']?div_adhost["']?[^>]*>.*?<\/div>/g, '');
-
-// 删除 .div_close_ads 广告
-modifiedBody = modifiedBody.replace(/<div[^>]*class=["']?div_close_ads["']?[^>]*>.*?<\/div>/g, '');
+// 遍历删除所有匹配的广告元素
+adClasses.forEach(className => {
+    const regex = new RegExp(`<div[^>]*class=["']?${className}["']?[^>]*>.*?<\\/div>`, "g");
+    modifiedBody = modifiedBody.replace(regex, '');
+});
 
 // 返回修改后的 HTML
 $done({ body: modifiedBody });
