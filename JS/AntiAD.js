853f2344bef86fe21b7c68589113f614e0e091bb
LOWERTOP <onone.so@gmail.com>
2025-03-11 18:42:56 +0800

    Update AntiAD.js

--- JS/AntiAD.js
+++ JS/AntiAD.js
@@ -1,12 +1,24 @@
 /**
  * Shadowrocket 移除 twmanga 相关网站的广告元素
- * 目标：删除 class="mobadsq" 的广告
+ * 目标：删除 class="mobadsq", "div_sub_adhost", "div_adhost", "div_close_ads" 的广告
  * 适用于 twmanga 的所有域名
  */
 
 // 解析 HTML 并删除目标广告元素
 const body = $response.body;
-const modifiedBody = body.replace(/<div[^>]*class=["']?mobadsq["']?[^>]*>.*?<\/div>/g, '');
+let modifiedBody = body;
+
+// 删除 .mobadsq 广告
+modifiedBody = modifiedBody.replace(/<div[^>]*class=["']?mobadsq["']?[^>]*>.*?<\/div>/g, '');
+
+// 删除 .div_sub_adhost 广告
+modifiedBody = modifiedBody.replace(/<div[^>]*class=["']?div_sub_adhost["']?[^>]*>.*?<\/div>/g, '');
+
+// 删除 .div_adhost 广告
+modifiedBody = modifiedBody.replace(/<div[^>]*class=["']?div_adhost["']?[^>]*>.*?<\/div>/g, '');
+
+// 删除 .div_close_ads 广告
+modifiedBody = modifiedBody.replace(/<div[^>]*class=["']?div_close_ads["']?[^>]*>.*?<\/div>/g, '');
 
 // 返回修改后的 HTML
 $done({ body: modifiedBody });
