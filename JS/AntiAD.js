40dd4bc7952f3d24c3f7f01f8753373218625372
LOWERTOP <onone.so@gmail.com>
2025-03-11 18:05:50 +0800

    Update AntiAD.js

--- JS/AntiAD.js
+++ JS/AntiAD.js
@@ -1,12 +1,12 @@
 /**
- * Shadowrocket 移除 twmanga.com 镶嵌的广告元素占位
+ * Shadowrocket 移除 twmanga 相关网站的广告元素
  * 目标：删除 class="mobadsq" 的广告
- * 作者: LOWERTOP
+ * 适用于 twmanga 的所有域名
  */
 
 // 解析 HTML 并删除目标广告元素
-const body = $response。body;
-const modifiedBody = body。replace(/<div[^>]*class=["']?mobadsq["']?[^>]*>.*?<\/div>/g， '');
+const body = $response.body;
+const modifiedBody = body.replace(/<div[^>]*class=["']?mobadsq["']?[^>]*>.*?<\/div>/g, '');
 
 // 返回修改后的 HTML
 $done({ body: modifiedBody });
