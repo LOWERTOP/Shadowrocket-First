6dabdc01fc67f7ff152a77fd11948b4fe6a939ac
LOWERTOP <onone.so@gmail.com>
2025-03-11 19:37:33 +0800

    Update AntiAD.js

--- JS/AntiAD.js
+++ JS/AntiAD.js
@@ -8,14 +8,20 @@
 const body = $response.body;
 let modifiedBody = body;
 
-// 需要删除的广告类名列表
-const adClasses = ["mobadsq", "div_sub_adhost", "div_adhost", "div_close_ads", "l-box"];
+// 删除 .mobadsq 广告
+modifiedBody = modifiedBody.replace(/<div[^>]*\bmobadsq\b[^>]*>[.\s\S]*?<\/div>/gi, '');
 
-// 更强的正则匹配方式，兼容多行、嵌套子元素等情况
-adClasses.forEach(className => {
-    const regex = new RegExp(`<div[^>]*\\b${className}\\b[^>]*>[\\s\\S]*?<\\/div>`, "g");
-    modifiedBody = modifiedBody.replace(regex, '');
-});
+// 删除 .div_sub_adhost 广告
+modifiedBody = modifiedBody.replace(/<div[^>]*\bdiv_sub_adhost\b[^>]*>[.\s\S]*?<\/div>/gi, '');
+
+// 删除 .div_adhost 广告
+modifiedBody = modifiedBody.replace(/<div[^>]*\bdiv_adhost\b[^>]*>[.\s\S]*?<\/div>/gi, '');
+
+// 删除 .div_close_ads 广告
+modifiedBody = modifiedBody.replace(/<div[^>]*\bdiv_close_ads\b[^>]*>[.\s\S]*?<\/div>/gi, '');
+
+// 删除 .l-box 广告
+modifiedBody = modifiedBody.replace(/<div[^>]*\bl-box\b[^>]*>[.\s\S]*?<\/div>/gi, '');
 
 // 返回修改后的 HTML
 $done({ body: modifiedBody });
