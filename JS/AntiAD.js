1ed3e4a997aa91a8c3a375064e308a02359e82a8
LOWERTOP <onone.so@gmail.com>
2025-03-11 23:50:32 +0800

    Update AntiAD.js

--- JS/AntiAD.js
+++ JS/AntiAD.js
@@ -1,8 +1,9 @@
 /**
  * Shadowrocket 移除 twmanga.com & baozimh.com 相关广告元素
  * 目标：
- * - 删除 twmanga 的 class="mobadsq", "div_sub_adhost", "div_adhost", "div_close_ads", "l-box", 
- * - 删除 baozimh 的 class="recommend" 和 h3[style="margin: 0 0 12px; padding: 0;"]
+ * - 删除 twmanga 的广告 class：mobadsq, div_sub_adhost, div_adhost, div_close_ads, l-box, m-page-bottom
+ * - 删除 baozimh 的广告 class：recommend, addthis-box
+ * - 删除 baozimh.com 的 h3[style="margin: 0 0 12px; padding: 0;"]
  * - 删除 twmanga.com 的 div[style="overflow:hidden; flex: 1;"]
  * 作者: LOWERTOP
  */
@@ -13,24 +14,17 @@ const url = $request.url;
 // 仅在 twmanga.com 和 baozimh.com 及其所有子域名下执行
 if (/https?:\/\/([^\/]+\.)?(twmanga|baozimh)\.com/.test(url)) {
     // 解析 HTML 并删除目标广告元素
-    const body = $response.body;
-    let modifiedBody = body;
+    let modifiedBody = $response.body;
 
-    // 定义要删除的广告 class 名称
+    // 定义所有要删除的广告 class 名称
     const adClasses = [
-        "mobadsq",          // twmanga 广告
-        "div_sub_adhost",   // twmanga 广告
-        "div_adhost",       // twmanga 广告
-        "div_close_ads",    // twmanga 广告
-        "l-box",            // twmanga 广告
-        "recommend"         // baozimh 推荐广告
+        "mobadsq", "div_sub_adhost", "div_adhost", "div_close_ads", "l-box", "m-page-bottom",  // twmanga 广告
+        "recommend", "addthis-box"  // baozimh 广告
     ];
 
-    // 循环删除多个广告元素
-    adClasses.forEach(adClass => {
-        const regex = new RegExp(`<div[^>]*class=["']?${adClass}["']?[^>]*>.*?<\\/div>`, "gi");
-        modifiedBody = modifiedBody.replace(regex, '');
-    });
+    // 统一匹配 class 进行删除
+    const regex = new RegExp(`<div[^>]*class=["']?(${adClasses.join('|')})["']?[^>]*>.*?<\\/div>`, "gi");
+    modifiedBody = modifiedBody.replace(regex, '');
 
     // 额外删除 baozimh.com 特定的 h3 标签广告
     modifiedBody = modifiedBody.replace(/<h3[^>]*style=["']?margin:\s?0\s?0\s?12px;\s?padding:\s?0;["']?[^>]*>.*?<\/h3>/gi, '');
