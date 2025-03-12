de93b2f6b6443f75aee346fce1811662089e980b
LOWERTOP <onone.so@gmail.com>
2025-03-12 22:33:02 +0800

    Update spotify-header.js

--- JS/spotify-header.js
+++ JS/spotify-header.js
@@ -1,12 +1,10 @@
 // Shadowrocket 脚本：删除 "If-None-Match" 请求头
-const url = $request.url;
 const targetPattern = /^https:\/\/spclient\.wg\.spotify\.com\/user-customization-service\/v1\/customize$/;
 
-if (targetPattern.test(url)) {
-    let modifiedHeaders = Object.assign({}, $request.headers); // 复制 headers 避免直接修改原始对象
-    delete modifiedHeaders["If-None-Match"]; // 删除请求头（大小写敏感）
+if (targetPattern.test($request.url)) {
+    let modifiedHeaders = { ...$request.headers }; // 使用展开运算符，避免修改原对象
+    delete modifiedHeaders["If-None-Match"]; // 删除目标请求头（区分大小写）
 
-    // 返回修改后的请求头
     $done({ headers: modifiedHeaders });
 } else {
     $done({});
