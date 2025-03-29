d13939649a179640721dd0f4e3bad27334fc1f54
LOWERTOP <onone.so@gmail.com>
2025-03-29 15:37:06 +0800

    Update WiFiCall_switch.js

--- JS/WiFiCall_switch.js
+++ JS/WiFiCall_switch.js
@@ -1,2 +1,8 @@
-const policy = $environment.params.split('=')[1];
-$done({ policy });
+const targetDomain = /^(.*\.)?gspe1-ssl\.ls\.apple\.com$/;
+const proxyGroup = "WiFiCall"; // 分组名称
+
+if (targetDomain.test($request.host)) {
+  $done({ policy: proxyGroup });
+} else {
+  $done();
+}
