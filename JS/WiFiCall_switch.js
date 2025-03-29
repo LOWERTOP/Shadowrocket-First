72966d5609111c50bc032468176017b0452b0fc6
LOWERTOP <onone.so@gmail.com>
2025-03-29 17:42:42 +0800

    Delete JS/WiFiCall_switch.js

--- JS/WiFiCall_switch.js
+++
@@ -1,8 +0,0 @@
-const targetDomain = /^(.*\.)?gspe1-ssl\.ls\.apple\.com$/;
-const proxyGroup = "WiFiCall"; // 分组名称
-
-if (targetDomain.test($request.host)) {
-  $done({ policy: proxyGroup });
-} else {
-  $done();
-}
