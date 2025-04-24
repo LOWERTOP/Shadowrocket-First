63227d1c78b0d979743509e54e298061b0f8ff60
LOWERTOP <onone.so@gmail.com>
2025-04-24 10:51:29 +0800

    Update spotify-header.js

--- JS/spotify-header.js
+++ JS/spotify-header.js
@@ -1,21 +1,20 @@
 // Spotify header rewrite script for Shadowrocket
-
 const url = $request.url;
-const targetUrl = "https://spclient.wg.spotify.com/user-customization-service/v1/customize";
+const targetPattern = /^https:\/\/spclient\.wg\.spotify\.com\/user-customization-service\/v1\/customize$/;
 
-if (url.indexOf(targetUrl) !== -1) {
-  // 如果URL匹配目标URL
-  let headers = $request.headers;
-  
-  // 删除if-none-match头
-  if (headers["if-none-match"]) {
-    delete headers["if-none-match"];
-    console.log("已删除if-none-match头");
-  }
-  
-  // 应用修改后的headers
-  $done({headers});
+if (targetPattern.test(url)) {
+    // 如果 URL 匹配目标路径
+    let headers = $request.headers;
+
+    // 删除 if-none-match 头部
+    if (headers["if-none-match"]) {
+        delete headers["if-none-match"];
+        console.log("已删除 if-none-match 头部");
+    }
+
+    // 返回修改后的 headers
+    $done({ headers });
 } else {
-  // 如果URL不匹配，不做任何更改
-  $done({});
+    // 如果 URL 不匹配，保持原样
+    $done({});
 }
