8d67495b0447f2ece1a5aacf74e87588f9a02d83
LOWERTOP <onone.so@gmail.com>
2025-04-30 13:09:57 +0800

    Update spotify-header.js

--- JS/spotify-header.js
+++ JS/spotify-header.js
@@ -1,20 +1,6 @@
-// Spotify header rewrite script for Shadowrocket
-const url = $request.url;
-const targetPattern = /^https:\/\/spclient\.wg\.spotify\.com\/user-customization-service\/v1\/customize$/;
-
-if (targetPattern.test(url)) {
-    // 如果 URL 匹配目标路径
-    let headers = $request.headers;
-
-    // 删除 if-none-match 头部
-    if (headers["if-none-match"]) {
-        delete headers["if-none-match"];
-        console.log("已删除 if-none-match 头部");
-    }
-
-    // 返回修改后的 headers
-    $done({ headers });
-} else {
-    // 如果 URL 不匹配，保持原样
-    $done({});
-}
+// Spotify header rewrite script
+(function() {
+  let headers = $request.headers;
+  delete headers['if-none-match']; 
+  $done({ headers: headers });
+})();
